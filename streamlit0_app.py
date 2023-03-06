512f1d2a88c678bd02a4519b233ef546aa42c333
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-03-06 13:50:06 +0900

    Update streamlit0_app.py

--- streamlit0_app.py
+++ streamlit0_app.py
@@ -1,88 +1,57 @@
+# 基本ライブラリ
 import streamlit as st
-import os
-import time
-from PIL import Image
 import numpy as np
-import tensorflow as tf
-import tensorflow_hub as hub
-import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
 
-# Constants
-SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
+# データセット読み込み
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
 
-# Load the model
-def load_model():
-    model = hub.load(SAVED_MODEL_PATH)
-    return model
+# 目標値
+df['target'] = iris.target
 
-model = load_model()
+# 目標値を数字から花の名前に変更
+df.loc[df['target'] == 0, 'target'] = 'setosa'
+df.loc[df['target'] == 1, 'target'] = 'versicolor'
+df.loc[df['target'] == 2, 'target'] = 'virginica'
 
-# Preprocess the input image to make it model ready
-def preprocess_image(image):
-  hr_image = np.array(image)
-  if hr_image.shape[-1] == 4:
-    hr_image = hr_image[...,:-1]
-  hr_size = (tf.convert_to_tensor(hr_image.shape[:-1]) // 4) * 4
-  hr_image = tf.image.crop_to_bounding_box(hr_image, 0, 0, hr_size[0], hr_size[1])
-  hr_image = tf.cast(hr_image, tf.float32)
-  return tf.expand_dims(hr_image, 0)
+# 予測モデル構築
+x = iris.data[:, [0, 2]] 
+y = iris.target
 
-# Save the output image
-def save_image(image, filename):
-    if not isinstance(image, Image.Image):
-        image = tf.clip_by_value(image, 0, 255)
-        image = Image.fromarray(tf.cast(image, tf.uint8).numpy())
-    if image.mode == 'RGBA':
-        image = image.convert('RGB')
-    image.save("%s.jpg" % filename)
+# ロジスティック回帰
+clf = LogisticRegression()
+clf.fit(x, y)
 
-# Plot the output image
-def plot_image(image, title=""):
-  image = np.asarray(image)
-  image = tf.clip_by_value(image, 0, 255)
-  image = Image.fromarray(tf.cast(image, tf.uint8).numpy())
-  st.image(image, caption=title, use_column_width=True)
+# サイドバー（入力画面）
+st.sidebar.header('Input Features')
 
-# Main function
-def app():
-  # Add title to the app
-  st.title("Super Resolution")
+sepalValue = st.sidebar.slider('sepal length (cm)', min_value=0.0, max_value=10.0, step=0.1)
+petalValue = st.sidebar.slider('petal length (cm)', min_value=0.0, max_value=10.0, step=0.1)
 
-  # Add sidebar to the app
-  st.sidebar.title("Settings")
-  contrast = st.sidebar.slider('Contrast', min_value=0.0, max_value=2.0, value=1.0, step=0.1)
-  st.sidebar.write('Contrast:', contrast)
-  brightness = st.sidebar.slider('Brightness', min_value=-0.5, max_value=0.5, value=0.0, step=0.05)
-  st.sidebar.write('Brightness:', brightness)
-  gamma = st.sidebar.slider('Gamma', min_value=0.1, max_value=10.0, value=1.0, step=0.1)
-  st.sidebar.write('Gamma:', gamma)
-  hue = st.sidebar.slider('Hue', min_value=-0.5, max_value=0.5, value=0.0, step=0.05)
-  st.sidebar.write('Hue:', hue)
+# メインパネル
+st.title('Iris Classifier')
+st.write('## Input Value')
 
-  # Add image uploader to the app
-  image_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+# インプットデータ（1行のデータフレーム）
+value_df = pd.DataFrame([],columns=['data','sepal length (cm)','petal length (cm)'])
+record = pd.Series(['data',sepalValue, petalValue], index=value_df.columns)
+value_df = value_df.append(record, ignore_index=True)
+value_df.set_index('data',inplace=True)
 
-  # If an image is uploaded
-  if image_file is not None:
-      # Display the original image
-      input_image = Image.open(image_file)
-      st.image(input_image, caption="Original Image", use_column_width=True)
-      
-      # Preprocess the input image for the model
-      hr_image = preprocess_image(input_image)
-      
-      # If the user clicks the "Super Resolution" button
-      if st.button('Super Resolution'):
-          if hr_image is not None:
-              # Generate the high resolution image using the ESRGAN model
-              fake_image = model(hr_image)
+# 入力値の値
+st.write(value_df)
 
-              # Display the super resolution image
-              st.image(fake_image[0], caption="Super Resolution Image", use_column_width=True)
+# 予測値のデータフレーム
+pred_probs = clf.predict_proba(value_df)
+pred_df = pd.DataFrame(pred_probs,columns=['setosa','versicolor','virginica'],index=['probability'])
 
-              # Save the super resolution image
-              save_image(fake_image[0], "super_resolution_image")
-        
-  # If no image is uploaded
-  else:
-      st.write("Please upload an image to use the app.")
+st.write('## Prediction')
+st.write(pred_df)
+
+# 予測結果の出力
+name = pred_df.idxmax(axis=1).tolist()
+st.write('## Result')
+st.write('このアイリスはきっと',str(name[0]),'です!')
