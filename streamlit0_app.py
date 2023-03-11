94f756fbae9bd17b5676db0afe7172e397468fb5
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-03-11 15:03:25 +0900

    Update streamlit0_app.py

--- streamlit0_app.py
+++ streamlit0_app.py
@@ -1,57 +1,32 @@
+%%writefile app.py 
+
 import streamlit as st
 import tensorflow as tf
 import tensorflow_hub as hub
-import numpy as np
-import PIL.Image
-import requests
-from io import BytesIO
 
-# 画風変換の関数
+
 @st.cache(show_spinner=False)
-def stylize(style_image, content_image):
+def load_image(image_file):
+    img = tf.io.decode_image(image_file.read())
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    img = tf.image.resize(img, [256, 256])
+    return img[tf.newaxis, :]
+
+
+@st.cache(show_spinner=False)
+def apply_style(content_image, style_image):
     hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
-    outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
-    stylized_image = outputs[0]
-    return np.array(stylized_image[0])
+    stylized_image = hub_module(tf.constant(content_image), tf.constant(style_image))[0]
+    return stylized_image
 
-# スタイル画像とコンテンツ画像の取得と前処理
-def get_style_content_images(style_image_file, content_image_file):
-    style_image = PIL.Image.open(style_image_file).convert('RGB').resize((256, 256))
-    content_image = PIL.Image.open(content_image_file).convert('RGB').resize((256, 256))
-    style_image = np.array(style_image) / 255.0
-    content_image = np.array(content_image) / 255.0
-    return style_image, content_image
 
-# Streamlit アプリケーション
-def app():
-    # ウィジェット
-    st.title('画風変換アプリ')
-    style_image_file_widget = st.sidebar.file_uploader('スタイル画像をアップロードしてください', type=['jpg', 'jpeg', 'png'])
-    content_image_file_widget = st.sidebar.file_uploader('コンテンツ画像をアップロードしてください', type=['jpg', 'jpeg', 'png'])
-    style_image_widget = st.empty()
-    content_image_widget = st.empty()
+st.title('画風変換アプリ')
 
-    # 画風変換を実行して結果を表示する関数
-    def on_change(change):
-        if change['type'] == 'change' and change['name'] == 'value':
-            style_image_file = style_image_file_widget.value
-            content_image_file = content_image_file_widget.value
-            if style_image_file is not None and content_image_file is not None:
-                # スタイル画像とコンテンツ画像の取得と前処理
-                style_image, content_image = get_style_content_images(style_image_file, content_image_file)
+content_image_file = st.file_uploader('コンテンツ画像をアップロードしてください')
+style_image_file = st.file_uploader('スタイル画像をアップロードしてください')
 
-                # スタイル画像とコンテンツ画像の表示
-                style_image_widget.image(style_image, caption='スタイル画像', use_column_width=True)
-                content_image_widget.image(content_image, caption='コンテンツ画像', use_column_width=True)
-
-                # 画風変換を実行
-                stylized_image = stylize(style_image, content_image)
-
-                # 結果の表示
-                st.image(stylized_image, caption='変換結果', use_column_width=True)
-
-    style_image_file_widget.observe(on_change)
-    content_image_file_widget.observe(on_change)
-
-if __name__ == '__main__':
-    app()
+if content_image_file and style_image_file:
+    content_image = load_image(content_image_file)
+    style_image = load_image(style_image_file)
+    stylized_image = apply_style(content_image, style_image)
+    st.image(stylized_image, caption='結果')    
