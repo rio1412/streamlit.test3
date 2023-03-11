a47e6ddf69c9f0f8ea2a7b32f5040feb6151c561
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-03-11 17:03:43 +0900

    Update streamlit0_app.py

--- streamlit0_app.py
+++ streamlit0_app.py
@@ -1,9 +1,6 @@
-import random
-from matplotlib.backends.backend_agg import RendererAgg
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
 # アプリのタイトルを表示
 st.title("Financial Data Visualization App")
@@ -21,44 +18,41 @@ if uploaded_file is not None:
     # 経常利益、営業利益率、株価のみのデータを抽出する
     subset_data = financial_data[["経常利益","営業利益率","株価"]]
 
-    # 経常利益と株価の散布図を描画する関数
-    def plot_scatter(x, y):
+    # グラフを描画する関数を定義
+    def plot_graph(x, y):
         fig, ax = plt.subplots()
         ax.scatter(x, y)
         ax.set_xlabel(x.name)
         ax.set_ylabel(y.name)
         ax.set_title("Scatter plot")
-        return fig
+        st.pyplot(fig)
 
-    # 経常利益と営業利益率の折れ線グラフを描画する関数
-    def plot_line(x, y):
-        fig, ax = plt.subplots()
-        ax.plot(x, y)
-        ax.set_xlabel(x.name)
-        ax.set_ylabel(y.name)
-        ax.set_title("Line plot")
-        return fig
+    # 経常利益、営業利益率、株価のみのデータを表示
+    st.write(subset_data)
 
-    # 純資産と売上高の成長率の棒グラフを描画する関数
-    def plot_bar(x, y):
-        fig, ax = plt.subplots()
-        ax.bar(x, y)
-        ax.set_xlabel(x.name)
-        ax.set_ylabel(y.name)
-        ax.set_title("Bar plot")
-        return fig
+    # 売上高の成長率を計算して表示
+    net_assets = financial_data["純資産"]
+    net_assets_shift = net_assets.shift(-1)
+    growth_rate = (net_assets - net_assets_shift) / net_assets_shift * 100
+    gr_dropna = growth_rate.dropna()
+    st.write("売上高の成長率: ", gr_dropna)
 
-    # 表示する図をランダムに選択する
-    figures = []
-    figure_functions = [plot_scatter, plot_line, plot_bar]
-    for i in range(3):
-        figure_func = random.choice(figure_functions)
-        figure = figure_func(subset_data.iloc[:,i], subset_data.iloc[:,(i+1)%3])
-        figures.append(figure)
+    # 加重平均を計算して表示
+    sum_wa = 0
+    count = 0
 
-    # 選択された図を表示する
-    for figure in figures:
-        canvas = FigureCanvas(figure)
-        with st.spinner('Plotting...'):
-            st.write(figure)
-        st.write('\n')
+    for year in reversed(gr_dropna):
+        count += 1
+        weight = year * count
+        sum_wa += weight
+
+    result = sum_wa / ((1/2)*count*(count+1))
+    st.write("加重平均: ", result)
+
+    # 経常利益と株価の相関係数を計算して表示
+    correlation = financial_data[["経常利益","株価"]]
+    corr_val = correlation.corr().iloc[0, 1]
+    st.write("経常利益と株価の相関係数: ", corr_val)
+
+    # 経常利益と株価の散布図を描画
+    plot_graph(subset_data["経常利益"], subset_data["株価"])
