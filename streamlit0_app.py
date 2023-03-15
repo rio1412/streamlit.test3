de2e201f028b9c2db2cb0bb8639b5695d9e0bc53
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-03-15 12:28:03 +0900

    Update streamlit0_app.py

--- streamlit0_app.py
+++ streamlit0_app.py
@@ -5,44 +5,40 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 
-def create_wordcloud(text):
-    # 形態素解析
+# テキストを分かち書きする関数
+def tokenize(text):
     t = Tokenizer()
-    words = t.tokenize(text)
-    # 単語の出現回数をカウント
-    word_count = {}
-    for word in words:
-        if word.surface not in word_count:
-            word_count[word.surface] = 1
-        else:
-            word_count[word.surface] += 1
-    # ワードクラウドの作成
-    wordcloud = WordCloud(
-        background_color="white",
-        font_path="0D10DE1D-5D14-4E5C-B492-0164B69F4AB4.png",
-        width=800,
-        height=800,
-        prefer_horizontal=1.0,
-        min_word_length=2,
-        max_font_size=120,
-        colormap='coolwarm',
-        mask=np.array(Image.open('./img/mask.png')),
-        contour_width=3,
-        contour_color='black'
-    ).generate_from_frequencies(word_count)
+    tokens = t.tokenize(text)
+    return [token.surface for token in tokens]
 
-    # ワードクラウドを表示
-    plt.figure(figsize=[8, 8])
-    plt.imshow(wordcloud, interpolation='bilinear')
+# ワードクラウドを作成する関数
+def create_wordcloud(words):
+    mask = np.array(Image.open('./img/mask.png'))
+    wordcloud = WordCloud(background_color="white",
+                          width=800, height=600,
+                          font_path='font/mplus-2p-regular.ttf',
+                          contour_width=2, contour_color='steelblue',
+                          mask=mask
+                          ).generate(' '.join(words))
+    plt.imshow(wordcloud)
     plt.axis("off")
-    st.pyplot()
+    plt.show()
+    st.pyplot(plt)
 
-def main():
-    st.title("JanomeとWordCloudを組み合わせたアプリ")
-    st.write("以下にテキストを入力してください。")
-    text = st.text_area("テキスト入力", "", height=200)
-    if st.button("ワードクラウドを生成"):
-        create_wordcloud(text)
+# Streamlitアプリの設定
+st.set_page_config(page_title="分かち書き＆ワードクラウドアプリ", page_icon=":pencil:", layout="wide")
 
-if __name__ == "__main__":
-    main()
+# テキスト入力
+st.sidebar.header("テキスト入力")
+text = st.sidebar.text_area("ここにテキストを入力してください。")
+
+# 分かち書きとワードクラウドの作成
+if text:
+    st.header("分かち書き結果")
+    words = tokenize(text)
+    st.write(words)
+
+    st.header("ワードクラウド")
+    create_wordcloud(words)
+else:
+    st.write("テキストを入力してください。")
