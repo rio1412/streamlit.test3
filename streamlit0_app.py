cb316d2e2c65914ba68a6b44b06d52f4bd4d00c9
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-03-15 12:44:00 +0900

    Update streamlit0_app.py

--- streamlit0_app.py
+++ streamlit0_app.py
@@ -12,19 +12,20 @@ def tokenize(text):
     return [token.surface for token in tokens]
 
 # ワードクラウドを作成する関数
-def create_wordcloud(words):
-    mask = wordcloud.to_file("./{}.png".format(img_name))
-
+def create_wordcloud(words, mask_image=None):
+    if mask_image is not None:
+        mask = np.array(mask_image)
+    else:
+        mask = None
     wordcloud = WordCloud(background_color="white",
                           width=800, height=600,
-                          font_path='font/mplus-2p-regular.ttf',
+                          font_path="./font/mplus-2p-regular.ttf",
                           contour_width=2, contour_color='steelblue',
                           mask=mask
                           ).generate(' '.join(words))
     plt.imshow(wordcloud)
     plt.axis("off")
-    plt.show()
-    st.pyplot(plt)
+    st.pyplot()
 
 # Streamlitアプリの設定
 st.set_page_config(page_title="分かち書き＆ワードクラウドアプリ", page_icon=":pencil:", layout="wide")
@@ -33,6 +34,10 @@ st.set_page_config(page_title="分かち書き＆ワードクラウドアプリ"
 st.sidebar.header("テキスト入力")
 text = st.sidebar.text_area("ここにテキストを入力してください。")
 
+# マスク画像アップロード
+st.sidebar.header("マスク画像")
+mask_image = st.sidebar.file_uploader("ここにマスク画像をドロップしてください", type=["jpg", "jpeg", "png"])
+
 # 分かち書きとワードクラウドの作成
 if text:
     st.header("分かち書き結果")
@@ -40,6 +45,10 @@ if text:
     st.write(words)
 
     st.header("ワードクラウド")
-    create_wordcloud(words)
+    if mask_image is not None:
+        mask = Image.open(mask_image)
+        create_wordcloud(words, mask_image=mask)
+    else:
+        create_wordcloud(words)
 else:
     st.write("テキストを入力してください。")
