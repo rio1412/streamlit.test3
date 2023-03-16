112030ee2872fc24e23afede3d37a106e1df1a53
rio1412 <118584074+rio1412@users.noreply.github.com>
2023-03-16 21:17:48 +0900

    Update streamlit0_app.py

--- streamlit0_app.py
+++ streamlit0_app.py
@@ -1,54 +1,17 @@
-import streamlit as st
-from janome.tokenizer import Tokenizer
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-from PIL import Image
-import numpy as np
-
-# テキストを分かち書きする関数
-def tokenize(text):
-    t = Tokenizer()
-    tokens = t.tokenize(text)
-    return [token.surface for token in tokens]
-
-# ワードクラウドを作成する関数
-def create_wordcloud(words, mask_image=None):
-    if mask_image is not None:
-        mask = np.array(mask_image)
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+        # 物体を検出する
+        objects = detect_objects(image)
+        # 検出された物体がある場合は、レシピを表示する
+        if len(objects) > 0:
+            st.write('以下の食材が検出されました:')
+            for obj in objects:
+                st.write('- ' + obj['label'])
+            st.write('以下のレシピをおすすめします:')
+            # TODO: 食材に合わせたレシピを検索して表示する
+        else:
+            st.write('食材が検出されませんでした。もう一度画像をアップロードしてください。')
     else:
-        mask = None
-    wordcloud = WordCloud(background_color="white",
-                          width=800, height=600,
-                          font_path="examples/fonts/SourceHanSerif/SourceHanSerifK-Light.otf",
-                          contour_width=2, contour_color='steelblue',
-                          mask=mask
-                          ).generate(' '.join(words))
-    plt.imshow(wordcloud)
-    plt.axis("off")
-    st.pyplot()
-
-# Streamlitアプリの設定
-st.set_page_config(page_title="分かち書き＆ワードクラウドアプリ", page_icon=":pencil:", layout="wide")
-
-# テキスト入力
-st.sidebar.header("テキスト入力")
-text = st.sidebar.text_area("ここにテキストを入力してください。")
-
-# マスク画像アップロード
-st.sidebar.header("マスク画像")
-mask_image = st.sidebar.file_uploader("ここにマスク画像をドロップしてください", type=["jpg", "jpeg", "png"])
-
-# 分かち書きとワードクラウドの作成
-if text:
-    st.header("分かち書き結果")
-    words = tokenize(text)
-    st.write(words)
-
-    st.header("ワードクラウド")
-    if mask_image is not None:
-        mask = Image.open(mask_image)
-        create_wordcloud(words, mask_image=mask)
-    else:
-        create_wordcloud(words)
-else:
-    st.write("テキストを入力してください。")
+        st.write('画像をアップロードしてください。')
+        
+if __name__ == '__main__':
+    main()
